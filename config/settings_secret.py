cc0b8d0bc726ce53bba76ef2792b26f0795ff08d
maroca <maoyam.logz@gmail.com>
2024-03-23 12:56:35 +0900

    ignoreの修正

---
+++ config/settings_secret.py
@@ -0,0 +1,7 @@
+# パスワードリセット用メール設定
+EMAIL_HOST = 'smtp.gmail.com'
+MAIL_PORT = 587
+EMAIL_HOST_USER = 'maruuca.com@gmail'
+EMAIL_HOST_PASSWORD = 'oedt lewj bfgk helx'
+EMAIL_USE_TLS = True
+EMAIL_FROM = 'NagoyaMeshi'
